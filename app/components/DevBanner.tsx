0f35df735ce2aa4012a1bae5b946cd16630e4cc4
Abdullah Almofleh <almofleh.abdullah@gmail.com>
2025-11-22 22:53:44 +0400

    fix: remove debug display of environment variables in `DevBanner` component

--- app/components/DevBanner.tsx
+++ app/components/DevBanner.tsx
@@ -8,7 +8,6 @@ const DevBanner = () => {
       <div className="fixed top-4 left-4 bg-red-500 text-white px-3 py-1 rounded-md text-xs font-medium z-50 shadow-lg">
         <span>DEV</span>
       </div>
-      <pre>{JSON.stringify(process.env, null, 2)}</pre>
     </>
   );
 };
