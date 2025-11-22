30f16f73c0c821e7a7fc7b98df7bba3ad9816c4d
Abdullah Almofleh <almofleh.abdullah@gmail.com>
2025-11-22 23:07:24 +0400

    fix: add display of environment variables in `<pre>` tag in `DevBanner` component

--- app/components/DevBanner.tsx
+++ app/components/DevBanner.tsx
@@ -8,6 +8,7 @@ const DevBanner = () => {
       <div className="fixed top-4 left-4 bg-red-500 text-white px-3 py-1 rounded-md text-xs font-medium z-50 shadow-lg">
         <span>DEV</span>
       </div>
+      <pre>{JSON.stringify(process.env, null, 2)}</pre>
     </>
   );
 };
